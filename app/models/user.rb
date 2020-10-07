8e993807e30ebb2856dea0287557bb15ef6ff14c
ar77777 <game_aran_777@yahoo.co.jp>
2020-10-07 17:55:24 +0900

    ユーザー機能実装

---
+++ app/models/user.rb
@@ -0,0 +1,24 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+        #  新規登録/ユーザー情報
+         validates :nickname, presence: true
+         validates :email, uniqueness: { case_sensitive: false }
+         validates :password, length: { minimum: 6 }
+         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+         validates_format_of :password, with: PASSWORD_REGEX 
+
+        #  新規登録/本人情報確認
+         with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
+          validates :last_name, presence: true
+          validates :first_name, presence: true
+         end
+         with_options presence: true, format: { with: /\A[ァ-ン]+\z/ } do
+          validates :last_name_kana, presence: true
+          validates :first_name_kana, presence: true
+         end
+         validates :birthday, presence: true
+end
