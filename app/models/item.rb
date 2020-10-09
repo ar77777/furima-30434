987d69867231c69ef26445b613e15afd68cb6e96
ar77777 <game_aran_777@yahoo.co.jp>
2020-10-09 12:00:45 +0900

    バリデーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,10 +3,11 @@ class Item < ApplicationRecord
   belongs_to :user
 
   # Validation
-  validates :name, :description, :category_id, :condition_id, :buyer_burden_id, :prefecture_id, :delivery_time_id, :image, presence: true
+  validates :name, :description,:image, presence: true
   validates :price, presence: true, numericality: { only_integer: true, greater_than: 300, less_than: 10_000_000 }
   PRICE_REGEX = /\A[0-9]+\z/.freeze
   validates_format_of :price, with: PRICE_REGEX
+  validates :category_id, :condition_id, :buyer_burden_id, :prefecture_id, :delivery_time_id, numericality: { other_than: 1 }
 
   # Activehash
   extend ActiveHash::Associations::ActiveRecordExtensions
@@ -15,8 +16,6 @@ class Item < ApplicationRecord
   belongs_to_active_hash :buyer_burden
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :delivery_time
-
-  validates :category_id, :condition_id, numericality: { other_than: 1 }
 
   # ActiveStorage
   has_one_attached :image
