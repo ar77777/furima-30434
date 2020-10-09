0fa746fb12d3e2634841e3eb14e7f56875c10b4e
ar77777 <game_aran_777@yahoo.co.jp>
2020-10-09 20:37:32 +0900

    rubocop実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -44,15 +44,11 @@ class ItemsController < ApplicationController
 
   def identification
     item = Item.find(params[:id])
-    unless item.user.id == current_user.id
-      redirect_to action: :index
-    end
+    redirect_to action: :index unless item.user.id == current_user.id
   end
 
   def login_confirmation
     item = Item.find(params[:id])
-    unless user_signed_in?
-      redirect_to action: :index
-    end
+    redirect_to action: :index unless user_signed_in?
   end
 end
