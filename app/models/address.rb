cda2baa968be8c207937e738413f4746952a8df6
ar77777 <game_aran_777@yahoo.co.jp>
2020-10-15 17:14:36 +0900

    コードの修正

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,5 +2,5 @@ class Address < ApplicationRecord
   belongs_to :purchase
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :perfecture
+  belongs_to_active_hash :prefecture
 end
