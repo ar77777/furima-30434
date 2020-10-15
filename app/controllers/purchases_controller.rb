182c7262a5fa7236e6d2cf370ce727f82cbbc467
ar77777 <game_aran_777@yahoo.co.jp>
2020-10-15 16:39:48 +0900

    コードのDRY化

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -2,14 +2,13 @@ class PurchasesController < ApplicationController
   before_action :authenticate_user!, only: :index
   before_action :identification, only: :index
   before_action :sale_check, only: :index
+  before_action :set_item, only: [:index, :create]
 
   def index
-    @item = Item.find(params[:item_id])
     @purchase_address = PurchaseAddress.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @purchase_address = PurchaseAddress.new(purchase_params)
     if @purchase_address.valid?
       pay_item
@@ -21,16 +20,18 @@ class PurchasesController < ApplicationController
   end
 
   private
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
 
   def purchase_params
     params.require(:purchase_address).permit(:postal, :prefecture_id, :city, :block, :building_name, :phone_number, :token).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
   def pay_item
-    item = Item.find(params[:item_id])
     Payjp.api_key = ENV['PAYJP_SECRET_KEY']
     Payjp::Charge.create(
-      amount: item.price,
+      amount: @item.price,
       card: purchase_params[:token],
       currency: 'jpy'
     )
